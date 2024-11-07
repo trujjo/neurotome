bd00b52ffa685d25ee276617c948d76838abf56b
Jon Trujillo <JonTrujillo@UTexas.edu>
2024-11-07 12:05:55 -0600

    Update app.py

--- app.py
+++ app.py
@@ -15,3 +15,19 @@ class Neo4jConnection:
         with self.driver.session() as session:
             result = session.run(query, parameters or {})
             return [record for record in result]
+
+from flask import Flask, render_template
+from neo4j import GraphDatabase
+
+app = Flask(__name__)
+
+# Neo4j connection configuration
+URI = "neo4j+s://4e5eeae5.databases.neo4j.io:7687"
+AUTH = ("neo4j", "Poconoco16!")
+
+@app.route('/')
+def index():
+    return render_template('visualization.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
