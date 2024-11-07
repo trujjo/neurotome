de8573199740e49cd509ea6a345fa7e878fed100
Jon Trujillo <JonTrujillo@UTexas.edu>
2024-11-07 11:16:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ load_dotenv()
 app = Flask(__name__)
 
 # Neo4j Configuration
-NEO4J_URI = os.getenv('NEO4J_URI', 'neo4j+s://4e5eeae5.databases.neo4j.io:7687')
+NEO4J_URI = os.getenv('NEO4J_URI', 'bolt://4e5eeae5.databases.neo4j.io:7687')
 NEO4J_USER = os.getenv('NEO4J_USER', 'neo4j')
 NEO4J_PASSWORD = os.getenv('NEO4J_PASSWORD', 'Poconoco16!')
 
