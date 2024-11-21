68310086e76b86578ff78856d21c716cbe58f1e2
Jon Trujillo <JonTrujillo@UTexas.edu>
2024-11-21 04:00:16 +0000

    1

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ CORS(app)
 # Neo4j connection configuration
 NEO4J_URI = os.getenv("NEO4J_URI", "bolt+s://4e5eeae5.databases.neo4j.io:7687")
 NEO4J_USER = os.getenv("NEO4J_USER", "neo4j")
-NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "Poconoco16!"))
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "Poconoco16!")
 
 # Initialize Neo4j driver
 driver = GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USER, NEO4J_PASSWORD))
