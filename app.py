a1b9173ce38a9f09d4749dbf2e161a30f968309b
Jon Trujillo <JonTrujillo@UTexas.edu>
2024-11-21 03:37:27 +0000

    1

--- app.py
+++ app.py
@@ -12,17 +12,14 @@ app = Flask(__name__)
 CORS(app)
 
 # Neo4j connection configuration
-NEO4J_URI = os.getenv("NEO4J_URI", "neo4j+s://4e5eeae5.databases.neo4j.io:7687")
+NEO4J_URI = os.getenv("NEO4J_URI", "bolt+s://4e5eeae5.databases.neo4j.io:7687")
 NEO4J_USER = os.getenv("NEO4J_USER", "neo4j")
 NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "Poconoco16!")
 
 driver = GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USER, NEO4J_PASSWORD))
 
 def get_neo4j_driver():
-    uri = "neo4j+s://4e5eeae5.databases.neo4j.io:7687"
-    user = "neo4j"
-    password = "Poconoco16!"
-    return GraphDatabase.driver(uri, auth=(user, password))
+    return driver
 
 @app.route('/')
 def index():
