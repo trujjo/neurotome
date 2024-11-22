9d1b203c6adad0fb448e79755144971225156e09
Jon Trujillo <JonTrujillo@UTexas.edu>
2024-11-22 01:17:39 +0000

    1

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+import numpy as np
+from neural_network import NeuralNetwork
+
+app = Flask(__name__)
+nn = NeuralNetwork()
+
+@app.route('/train', methods=['POST'])
+def train():
+    data = request.get_json()
+    inputs = np.array(data['inputs'])
+    targets = np.array(data['targets'])
+    epochs = data.get('epochs', 1000)
+    
+    nn.train(inputs, targets, epochs)
+    return jsonify({'status': 'success'})
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    inputs = np.array(data['inputs'])
+    
+    prediction = nn.predict(inputs)
+    return jsonify({'prediction': prediction.tolist()})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
