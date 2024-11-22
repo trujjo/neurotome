9de0b8912e220bfa90af03e409dd6b9ad05907b3
Jon Trujillo <JonTrujillo@UTexas.edu>
2024-11-22 01:43:13 +0000

    1

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
-from app import app
+try:
+    from .app import app
+except ImportError:
+    try:
+        from app import app
+    except ImportError:
+        raise ImportError("Could not import app. Make sure app.py exists in the correct location.")
 
 if __name__ == "__main__":
     app.run(debug=True)
