81763a43eb9f559e5ab8928ed9faebd8d735784c
Jon Trujillo <JonTrujillo@UTexas.edu>
2025-01-03 23:45:19 -0600

    Delete public directory

--- public/js/brain-viewer.js
+++
@@ -1,5 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-    const viewer = document.getElementById('brain-viewer');
-    viewer.innerHTML = '<p>3D Brain Viewer will be implemented here</p>';
-});
