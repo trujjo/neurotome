3a4413ba676b8c1a3093707a28748c046d9f8488
Jon Trujillo <JonTrujillo@UTexas.edu>
2025-01-03 18:58:15 -0600

    Update brain-viewer.js

---
+++ public/js/brain-viewer.js
@@ -0,0 +1,5 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const viewer = document.getElementById('brain-viewer');
+    viewer.innerHTML = '<p>3D Brain Viewer will be implemented here</p>';
+});
